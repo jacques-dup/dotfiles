898869cf6faa34b69c8c562f2dce3cc062b9f223
jacques-dup <jacques.duplessis@kurtosys.com>
2020-12-31 18:28:38 +0200

    don't clone dracula

--- scripts/pulls.sh
+++ scripts/pulls.sh
@@ -1,8 +1,2 @@
 cd ~/dotfiles/theme/
-mkdir dracula
-cd dracula
-git clone https://github.com/dracula/iterm.git iterm
-git clone https://github.com/dracula/zsh.git zsh
-
-cd ~/dotfiles/theme/
 git clone https://github.com/ohmyzsh/ohmyzsh.git oh-my-zsh
